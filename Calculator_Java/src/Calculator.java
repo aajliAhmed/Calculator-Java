050bf477032503ef25eb63acb29cb299bf2b4f41
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-01-10 15:19:26 +0100

    ajouter les nombres negatif

--- Calculator_Java/src/Calculator.java
+++ Calculator_Java/src/Calculator.java
@@ -7,9 +7,9 @@ public class Calculator implements ActionListener {
 	JFrame frame;
 	JTextField textField;
 	JButton[] numberButtons = new JButton[10];
-	JButton[] functionButtons = new JButton[8];
+	JButton[] functionButtons = new JButton[9];
 	JButton addButton,subButton,mulButton,divButton;
-	JButton decButton, equButton,delButton, clrButton;
+	JButton decButton, equButton,delButton, clrButton ,negButton;
 	JPanel panel;
 	
 	Font myFont = new Font("Ink Free",Font.BOLD,30);
@@ -37,6 +37,7 @@ public class Calculator implements ActionListener {
 		equButton = new JButton("=");
 		delButton = new JButton("delete");
 		clrButton = new JButton("clear");
+		negButton = new JButton("(-)");
 		
 		functionButtons[0] = addButton;
 		functionButtons[1] = subButton;
@@ -46,8 +47,9 @@ public class Calculator implements ActionListener {
 		functionButtons[5] = equButton;
 		functionButtons[6] = delButton;
 		functionButtons[7] = clrButton;
+		functionButtons[8] = negButton;
 		
-		for(int i=0;i<8;i++) {
+		for(int i=0;i<9;i++) {
 			functionButtons[i].addActionListener(this);
 			functionButtons[i].setFont(myFont);
 			functionButtons[i].setFocusable(false);
@@ -60,8 +62,9 @@ public class Calculator implements ActionListener {
 			numberButtons[i].setFocusable(false);
 		}
 		
-		delButton.setBounds(50,430,145,50);
-		clrButton.setBounds(205,430,145,50);
+		negButton.setBounds(50,430,100,50);
+		delButton.setBounds(150,430,100,50);
+		clrButton.setBounds(250,430,100,50);
 		
 		panel = new JPanel();
 		panel.setBounds(50, 100, 300, 300);
@@ -87,6 +90,7 @@ public class Calculator implements ActionListener {
 		
 		
 		frame.add(panel);
+		frame.add(negButton);
 		frame.add(delButton);
 		frame.add(clrButton);
 		frame.add(textField);
